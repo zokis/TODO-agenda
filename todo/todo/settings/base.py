87665275d921772bde28d7895227e2e73343f633
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-11-17 01:18:23 -0200

    comenta django_extensions

--- todo/todo/settings/base.py
+++ todo/todo/settings/base.py
@@ -196,7 +196,7 @@ DJANGO_APPS = (
 THIRD_PARTY_APPS = (
     # Database migration helpers:
     #'south',
-    'django_extensions',
+    #'django_extensions',
 )
 
 # Apps specific for this project go here.
