18674dd38b11884f55097964220c6a3139a54914
Tiago Cambará <tiago.cambara21@gmail.com>
2014-11-17 11:30:21 -0200

    melhorando as urls

--- todo/todo/agenda/urls.py
+++ todo/todo/agenda/urls.py
@@ -14,14 +14,15 @@ urlpatterns = patterns(
         CalendarioView.as_view(),
         name='calendario'
     ),
-    url(r'^evento_form/$', 'evento_form', name='evento_form'),
-    url(r'^meus_eventos/$', 'meus_eventos', name='meus_eventos'),
-    url(r'^eventos/$', 'eventos', name='eventos'),
-    url(r'^evento_form/(?P<pk>\d+)/$', 'evento_form', name='evento_form'),
+    url(r'^evento/form/$', 'evento_form', name='evento_form'),
+    url(r'^evento/meus/$', 'meus_eventos', name='meus_eventos'),
+    url(r'^evento/list/$', 'eventos', name='eventos'),
+    url(r'^evento/publico/form/(?P<pk>\d+)/$', 'evento_form', {'publico': True}, name='evento_publico_form'),
+    url(r'^evento/form/(?P<pk>\d+)/$', 'evento_form', {'publico': False}, name='evento_form'),
+    url(r'^evento/delete/(?P<pk>\d+)/$', 'evento_delete', name="evento_delete"),
 
-
-    url(r'^list/departamento/$', 'departamento_list', name="departamento_list"),
-    url(r'^create/departamento/$', 'departamento_create', name="departamento_form"),
-    url(r'^update/departamento/(?P<pk>\d+)/$', 'departamento_update', name="departamento_form"),
-    url(r'^delete/departamento/(?P<pk>\d+)/$', 'departamento_delete', name="departamento_delete"),
+    url(r'^departamento/list/$', 'departamento_list', name="departamento_list"),
+    url(r'^departamento/form/$', 'departamento_create', name="departamento_form"),
+    url(r'^departamento/form/(?P<pk>\d+)/$', 'departamento_update', name="departamento_form"),
+    url(r'^departamento/delete/(?P<pk>\d+)/$', 'departamento_delete', name="departamento_delete"),
 )
